27e253681bc66494e37bf90f0941564d4d889ce8
James Swandale <sinfrax@gmail.com>
2015-11-12 17:30:51 +0000

    Revert "returning deleted result, 404 record non existant"
    This reverts commit 9ebaff92af6a2c0cca26439d8243ae8101e5487a.

--- test/integration/deleteDictionary.spec.js
+++ test/integration/deleteDictionary.spec.js
@@ -12,9 +12,9 @@ describe.only('[INTEGRATION] Delete Dictionary', function(){
 
 
   beforeEach(function(){
-    this.dictName = "dict1"
-    this.method = "delete"
     this.getUrl = function() { return '/api/v1.0/users/myUuid/dictionaries/'+this.dictName+'.json'; }
+    this.method = "delete"
+    this.dictName = "dict1"
 
     this.expectedBody = {};
     this.dict1 = { "name" : "dict1", "field1" : "value1", "meta" : { "uuid" : "myUuid", "scope" : "users" } };
@@ -51,25 +51,14 @@ describe.only('[INTEGRATION] Delete Dictionary', function(){
           });
        });
 
-        it('returns the removed element as json', function(done){
-          var expectedBody = { name: "dict1", field1: "value1" };
+        it('returns the removed element as json', function(){
 
-          this.doRequest(function(req){
-            var correctBody = function(res) {
-              assert.deepEqual(res.body, expectedBody);
-            };
-
-            req.expect(correctBody).end(done);
-          });
         });
       });
 
       describe('when the given record does not exist', function(){
-        it('returns a 404', function(done){
-          this.dictName = 'nonExistingDict';
-          this.doRequest(function(req){
-            req.expect(404, done);
-          });
+        it('returns a 404', function(){
+
         });
       });
     });
