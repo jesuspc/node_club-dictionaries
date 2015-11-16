aa7d451da4ac674fffac75d2d76adba088be5c16
James Swandale <jamesswandale@workshare.com>
2015-11-16 12:40:15 +0000

    renamed payloads, fixed formatting, added func to parameterize upsert

--- test/integration/createDictionary.spec.js
+++ test/integration/createDictionary.spec.js
@@ -18,38 +18,38 @@ describe('[INTEGRATION] Create Dictionary', function(){
     this.expectedBody = {};
     this.dict1 = { "name" : "dict1", "field1" : "value1", "meta" : { "uuid" : "myUuid", "scope" : "users" } };
     this.dict1Overwrite = { "name" : "dict1", "field1" : "value2", "meta" : { "uuid" : "myUuid", "scope" : "users" } };
-    this.dictUnnamedPayload = {"unnamedField" : "unnamedValue", "meta" : { "uuid" : "myUuid", "scope" : "accounts" } };
+    this.unnamedDict = {"unnamedField" : "unnamedValue", "meta" : { "uuid" : "myUuid", "scope" : "accounts" } };
     this.dictionaries = [this.dict1];
   });
 
 
   describe('when user logged in', function(){
     describe('when successful authorization', function(){
-      describe('when payload provided', function(){
+      describe('when dictionary provided', function(){
         beforeEach(function(){
-          this.dictName = 'unnamedPayload';
+          this.dictName = 'unnamedDict';
         })
 
         it('returns a 200', function(done){
-          that = this
+          var that = this;
             this.doRequest(function(req){
-              req.send(that.dictUnnamedPayload);
+              req.send(that.unnamedDict);
               req.expect(200, done);
             })
         });
 
-        it('creates a record with by merging the given name and the payload', function(done){
-          that = this;
-          var expectedPayload = {"name": "unnamedPayload", "unnamedField" : "unnamedValue"};
+        it('creates a record with by merging the given name and the dictionary', function(done){
+          var that = this;
+          var expectedDictionary = {"name": "unnamedDict", "unnamedField" : "unnamedValue"};
 
           this.doRequest(function(req){
-            req.send(that.dictUnnamedPayload)
+            req.send(that.unnamedDict)
             req.end(function(){
               dbConnection().then(function(db){
                 db.collection('dictionaries').findOne({name: that.dictName}).then(function(dict){
                     try{
-                      assert.deepEqual(dict.name, expectedPayload.name);
-                      assert.deepEqual(dict.unnamedField, expectedPayload.unnamedField);
+                      assert.deepEqual(dict.name, expectedDictionary.name);
+                      assert.deepEqual(dict.unnamedField, expectedDictionary.unnamedField);
                       done();
                     } catch(err) {
                       done(err);
@@ -61,37 +61,37 @@ describe('[INTEGRATION] Create Dictionary', function(){
         });
 
         it('returns the created element as json', function(done){
-          var expectedBody = { name: "unnamedPayload", unnamedField: "unnamedValue" };
-          that = this;
+          var expectedBody = { name: "unnamedDict", unnamedField: "unnamedValue" };
+          var that = this;
           this.doRequest(function(req){
-            req.send(that.dictUnnamedPayload)
+            req.send(that.unnamedDict)
             var correctBody = function(res) {
               assert.deepEqual(res.body, expectedBody);
             };
-
+            
             req.expect(correctBody).end(done);
           });
         });
       });
 
-      describe('when no payload provided', function(done){
+      describe('when no dictionary provided', function(done){
         it('returns a 200', function(done){
-          that = this
-            this.doRequest(function(req){
-              req.expect(200, done);
-            })
+          var that = this;
+          this.doRequest(function(req){
+            req.expect(200, done);
+          });
         });
 
         it('creates a record with by with the given name', function(done){
-          that = this;
-          var expectedPayload = {"name": "newDict"};
+          var that = this;
+          var expectedDictionary = {"name": "newDict"};
 
           this.doRequest(function(req){
             req.end(function(){
               dbConnection().then(function(db){
                 db.collection('dictionaries').findOne({name: that.dictName}).then(function(dict){
                     try{
-                      assert.deepEqual(dict.name, expectedPayload.name);
+                      assert.deepEqual(dict.name, expectedDictionary.name);
                       done();
                     } catch(err) {
                       done(err);
@@ -104,7 +104,7 @@ describe('[INTEGRATION] Create Dictionary', function(){
 
         it('returns the created element as json', function(done){
           var expectedBody = {name: "newDict"};
-          that = this;
+          var that = this;
           this.doRequest(function(req){
             var correctBody = function(res) {
               assert.deepEqual(res.body, expectedBody);
@@ -118,7 +118,7 @@ describe('[INTEGRATION] Create Dictionary', function(){
       describe('when there was a previous entry with the given name', function(){
         it('overrides the previous entry', function(done){
           var expectedBody = { "name" : "dict1", "field1" : "value2"};
-          that = this;
+          var that = this;
 
           this.doRequest(function(req){
             req.send(that.dict1Overwrite)
